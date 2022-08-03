b2da8780db969ed2895beb560ce7c583bc8a7ab3
Hiroki Murakami <a2241hm@aiit.ac.jp>
2022-08-03 23:23:10 +0900

    スクリプトおよびテストスクリプト新規作成

--- kadai4_main.sh
+++ kadai4_main.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 #######################################
 # shell name  : kadai4_main.sh
@@ -6,20 +6,69 @@
 # Date        : 2021/08/06
 #######################################
 
-
+## 入力チェック
+# 引数チェック
 if [ $# -ne 2 ]; then
-echo "input 2 argments" 1>&2 exit 1
+	echo "2つの自然数を入力してください" 1>&2 
+exit 1
 fi
 
+# 自然数チェック1 数字以外の場合、終了ステータスが2以上になる 
 expr $1 + $2 > /dev/null 2>&1 
-if [ $?-ge2 ]; then
-	echo "input natural number" 1>&2
+if [ $? -ge 2 ]; then
+	echo "自然数を入力されていません" 1>&2
 	exit 1
 fi
 
-if [ $1-eq$2 ]; then
-	echo "$1 is equal to $2"
-else
-	echo "$1 is not equal to $2"
+# 自然数チェック2 引数にマイナスが存在しないかチェック
+for num in $@
+do
+	if [ $num -lt 0 ]; then
+		echo "マイナスが入力されています" 1>&2
+		exit 1
+	fi
+done
+
+# 自然数チェック3 引数に0が存在しないかチェック
+for num in $@
+do
+	if [ $num -eq 0 ]; then
+		echo "0が入力されています" 1>&2
+		exit 1
+	fi
+done
+
+# 入力値のセット
+m=$1
+n=$2
+
+# 先頭のメッセージ出力
+HEADER(){
+	echo "入力された "$m" と "$n" の"
+}
+
+# ユークリッドの互除法を使った最大公約数の計算
+# m=$1 n=$2  rを余りとする
+# $mが大きい場合と小さい場合の2パターンを用意する。
+if [ $m -gt $n ]; then       # $mが大きい場合
+	HEADER                   # ヘッダー出力
+    r=`expr $m % $n`
+    while [ $r != 0 ]
+    do
+        m=$n
+        n=$r
+        r=`expr $m \% $n`
+    done
+    echo "最大公約数は "$n" です。"
+else                         # $nが大きい場合
+	HEADER                   # ヘッダー出力
+    r=`expr $n \% $m`
+    while [ $r != 0 ]
+    do
+        n=$m
+        m=$r
+        r=`expr $n \% $m`
+    done
+    echo "最大公約数は "$m" です。"
 fi
 
--- kadai4_test.sh
+++ kadai4_test.sh
@@ -6,20 +6,68 @@
 # Date        : 2021/08/06
 #######################################
 
+## 初期設定
+ans="/tmp/$$-ans"
+result="/tmp/$$-result"
+err="/tmp/$$-error"
 
-if [ $# -ne 2 ]; then
-echo "input 2 argments" 1>&2 exit 1
-fi
+## テスト開始
 
-expr $1 + $2 > /dev/null 2>&1 
-if [ $?-ge2 ]; then
-	echo "input natural number" 1>&2
-	exit 1
-fi
+## 正常系
+# test1-1 :引数1の方が大きい場合
+./kadai4_main.sh 28 20 > ${result}
+echo "4" > ${ans}
+diff ${result} ${ans} || echo "test1-1 :引数1の方が大きい場合エラー" >> ${err}
 
-if [ $1-eq$2 ]; then
-	echo "$1 is equal to $2"
-else
-	echo "$1 is not equal to $2"
+# test1-2 :引数2の方が大きい場合
+./kadai4_main.sh 12 60 > ${result}
+echo "4" > ${ans}
+diff ${result} ${ans} || echo "test1-2 :引数2の方が大きい場合エラー" >> ${err}
+
+# test1-3 :比較的桁数の大きい自然数の場合
+./kadai4_main.sh 1905192228 5456037299 > ${result}
+echo "1" > ${ans}
+diff ${result} ${ans} || echo "test1-3 :比較的桁数の大きい自然数の場合エラー" >> ${err}
+
+## 異常系
+# test2-1 :引数なしの場合
+./kadai4_main.sh > ${result}
+echo "2つの自然数を入力してください" > ${ans}
+diff ${result} ${ans} || echo "test2-1 :引数なしの場合エラー" >> ${err}
+
+# test2-2 :引数が1つだけの場合
+./kadai4_main.sh 12 > ${result}
+echo "2つの自然数を入力してください" > ${ans}
+diff ${result} ${ans} || echo "test2-2 :引数が1つだけの場合エラー" >> ${err}
+
+# test2-3 :引数が3つある場合
+./kadai4_main.sh 12 24 66 > ${result}
+echo "2つの自然数を入力してください" > ${ans}
+diff ${result} ${ans} || echo "test2-3 :引数が3つある場合エラー" >> ${err}
+
+# test2-4 :引数２つで文字列の場合
+./kadai4_main.sh "aaa" "bbb" > ${result}
+echo "2つの自然数を入力してください" > ${ans}
+diff ${result} ${ans} || echo "test2-4 :引数２つで文字列の場合エラー" >> ${err}
+
+# test2-5 :引数の1つにマイナスが存在する場合
+./kadai4_main.sh -12 36 > ${result}
+echo "マイナスが入力されています" > ${ans}
+diff ${result} ${ans} || echo "test2-5 :引数の1つにマイナスが存在する場合エラー" >> ${err}
+
+# test2-6 :引数1に0が含まれる場合
+./kadai4_main.sh 0 36 > ${result}
+echo "0が入力されています" > ${ans}
+diff ${result} ${ans} || echo "test2-6 :引数1に0が含まれる場合エラー" >> ${err}
+
+# test2-7 :引数2に0が含まれる場合
+./kadai4_main.sh 36 0 > ${result}
+echo "0が入力されています" > ${ans}
+diff ${result} ${ans} || echo "test2-7 :引数2に0が含まれる場合エラー" >> ${err}
+
+if [ -f ${err} ]; then # エラーログが一つでもあれば
+	cat ${err}         # 中身を表示して
+	rm /tmp/$$-*	   # 後片付けをして
+	exit 1			   # 終了
 fi
 
